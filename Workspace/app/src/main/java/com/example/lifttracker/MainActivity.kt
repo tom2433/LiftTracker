5588df569059b2849561421068fa27526a7f3ad8
tom2433 <tom.2433.england@gmail.com>
2026-05-22 15:13:05 -0400

    completed task manager practice

--- Workspace/app/src/main/java/com/example/lifttracker/MainActivity.kt
+++ Workspace/app/src/main/java/com/example/lifttracker/MainActivity.kt
@@ -22,11 +22,13 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lifttracker.ui.theme.LiftTrackerTheme
+import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {        // onCreate = main
@@ -34,29 +36,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LiftTrackerTheme {
-//                // surface container using background color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    GreetingImage(
-//                        message = "Hello, World!",
-//                        from = "Bottom text"
-//                    )
-//                }
-
-                val image = painterResource(R.drawable.banner)
-                val title = stringResource(R.string.jetpack_compose_tutorial_title)
-                val intro = stringResource(R.string.jetpack_compose_tutorial_intro)
-                val body = stringResource(R.string.jetpack_compose_tutorial_body)
-
                 // surface container using background color from the theme
+                val graphic = painterResource(R.drawable.check)
+                val heading = "All tasks completed"
+                val subHeading = "Nice Work!"
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // add ArticleScreen here
-                    ArticleScreen(image, title, intro, body)
+                    AlertScreen(graphic, heading, subHeading)
                 }
             }
         }
@@ -64,106 +53,101 @@ class MainActivity : ComponentActivity() {
 }
 
 //@Composable
-//fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+//fun ArticleScreen(bannerImg: Painter, title: String, intro: String, body: String, modifier: Modifier = Modifier) {
 //    Column(
-//        verticalArrangement = Arrangement.Center,
-//        modifier = modifier
+//        modifier = modifier.fillMaxSize(),
+//        verticalArrangement = Arrangement.Top
 //    ) {
-//        Text(
-//            text = message,
-////            modifier = modifier.padding(24.dp)
-//            fontSize = 100.sp,
-//            lineHeight = 116.sp,
-//            textAlign = TextAlign.Center
-//        )
-//        Text(
-//            text = from,
-//            fontSize = 36.sp,
-//            modifier = Modifier
-//                .padding(16.dp)
-//                .align(alignment = Alignment.CenterHorizontally)
-//        )
-//    }
-//}
-//
-//@Composable
-//fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
-//    val image = painterResource(R.drawable.background)
-//    Box(modifier = modifier.fillMaxSize()) {
 //        Image(
-//            painter = image,
+//            painter = bannerImg,
 //            contentDescription = null,
-//            modifier = Modifier.fillMaxSize(),
-//            contentScale = ContentScale.Crop,
-//            alpha = 0.5F
+//            modifier = Modifier.fillMaxWidth()
 //        )
-//        GreetingText(
-//            message = message,
-//            from = from,
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(8.dp)
+//        Text(
+//            // 24sp font size, 16dp padding (all)
+//            text = title,
+//            fontSize = 24.sp,
+//            modifier = Modifier.padding(16.dp)
 //        )
-//    }
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    LiftTrackerTheme {
-//        GreetingImage(stringResource(R.string.hello_world), stringResource(R.string.bottom_text))
+//        Text(
+//            // default font size, 16dp padding (start and end), and justify text align
+//            text = intro,
+//            textAlign = TextAlign.Justify,
+//            modifier = Modifier.padding(
+//                start = 16.dp,
+//                end = 16.dp
+//            )
+//        )
+//        Text(
+//            // default font size, 16dp padding (all), justify text align
+//            text = body,
+//            textAlign = TextAlign.Justify,
+//            modifier = Modifier.padding(16.dp)
+//        )
 //    }
 //}
 
 @Composable
-fun ArticleScreen(bannerImg: Painter, title: String, intro: String, body: String, modifier: Modifier = Modifier) {
+fun AlertScreen(graphic: Painter, heading: String, subHeading: String, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        // checkmark graphic
         Image(
-            painter = bannerImg,
-            contentDescription = null,
-            modifier = Modifier.fillMaxWidth()
+            painter = graphic,
+            contentDescription = null
         )
+        // heading: bold, padding (24dp top, 8dp bottom)
         Text(
-            // 24sp font size, 16dp padding (all)
-            text = title,
-            fontSize = 24.sp,
-            modifier = Modifier.padding(16.dp)
-        )
-        Text(
-            // default font size, 16dp padding (start and end), and justify text align
-            text = intro,
-            textAlign = TextAlign.Justify,
+            text = heading,
+            fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(
-                start = 16.dp,
-                end = 16.dp
+                top = 24.dp,
+                bottom = 8.dp
             )
         )
+        // sub heading: 16sp font size
         Text(
-            // default font size, 16dp padding (all), justify text align
-            text = body,
-            textAlign = TextAlign.Justify,
-            modifier = Modifier.padding(16.dp)
+            text = subHeading,
+            fontSize = 16.sp
         )
     }
 }
 
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun ArticlePreview() {
+//    LiftTrackerTheme {
+//        val image = painterResource(R.drawable.banner)
+//        val title = stringResource(R.string.jetpack_compose_tutorial_title)
+//        val intro = stringResource(R.string.jetpack_compose_tutorial_intro)
+//        val body = stringResource(R.string.jetpack_compose_tutorial_body)
+//
+//        Surface(
+//            modifier = Modifier.fillMaxSize(),
+//            color = MaterialTheme.colorScheme.background
+//        ) {
+//            ArticleScreen(image, title, intro, body)
+//        }
+//    }
+//}
+
 @Preview(showBackground = true)
 @Composable
-fun ArticlePreview() {
+fun TasksCompletedPreview() {
     LiftTrackerTheme {
-        val image = painterResource(R.drawable.banner)
-        val title = stringResource(R.string.jetpack_compose_tutorial_title)
-        val intro = stringResource(R.string.jetpack_compose_tutorial_intro)
-        val body = stringResource(R.string.jetpack_compose_tutorial_body)
+        val graphic = painterResource(R.drawable.check)
+        val heading = "All tasks completed"
+        val subHeading = "Nice Work!"
 
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            ArticleScreen(image, title, intro, body)
+            AlertScreen(graphic, heading, subHeading)
         }
     }
 }
