170ef789cb65d0ee748ce99460149490323b1ed1
tom2433 <tom.2433.england@gmail.com>
2026-05-26 09:25:17 -0400

    completed Compose Quadrant Practice

--- Workspace/app/src/main/java/com/example/lifttracker/MainActivity.kt
+++ Workspace/app/src/main/java/com/example/lifttracker/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -19,17 +19,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.lifttracker.ui.theme.LiftTrackerTheme
-import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {        // onCreate = main
@@ -43,73 +37,80 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     // put grid stuff here
+                    GridScreen()
                 }
             }
         }
     }
 }
 
-//@Composable
-//fun AlertScreen(graphic: Painter, heading: String, subHeading: String, modifier: Modifier = Modifier) {
-//    Column(
-//        modifier = modifier.fillMaxSize(),
-//        verticalArrangement = Arrangement.Center,
-//        horizontalAlignment = Alignment.CenterHorizontally
-//    ) {
-//        // checkmark graphic
-//        Image(
-//            painter = graphic,
-//            contentDescription = null
-//        )
-//        // heading: bold, padding (24dp top, 8dp bottom)
-//        Text(
-//            text = heading,
-//            fontWeight = FontWeight.Bold,
-//            modifier = Modifier.padding(
-//                top = 24.dp,
-//                bottom = 8.dp
-//            )
-//        )
-//        // sub heading: 16sp font size
-//        Text(
-//            text = subHeading,
-//            fontSize = 16.sp
-//        )
-//    }
-//}
-
 @Composable
-fun GridScreen(modifier: Modifier = Modifier) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Start
+fun GridElement(heading: String, body: String, color: Color, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .background(color = color)
+            .padding(16.dp)
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            modifier = Modifier.weight(0.5f),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-        }
+        // header
+        Text(
+            text = heading,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(
+                bottom = 16.dp
+            )
+        )
+        // body
+        Text(
+            text = body,
+            textAlign = TextAlign.Justify
+        )
     }
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun TasksCompletedPreview() {
-//    LiftTrackerTheme {
-//        val graphic = painterResource(R.drawable.check)
-//        val heading = "All tasks completed"
-//        val subHeading = "Nice Work!"
-//
-//        Surface(
-//            modifier = Modifier.fillMaxSize(),
-//            color = MaterialTheme.colorScheme.background
-//        ) {
-//            AlertScreen(graphic, heading, subHeading)
-//        }
-//    }
-//}
+@Composable
+fun GridScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().weight(0.5f),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            GridElement(
+                "Text Composable",
+                "Displays text and follows the recommended Material Design guidelines.",
+                Color(0xFFEADDFF),
+                Modifier.weight(0.5f)
+            )
+            GridElement(
+                "Image composable",
+                "Creates a composable that lays out and draws a given Painter class object.",
+                Color(0xFFD0BCFF),
+                Modifier.weight(0.5f)
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth().weight(0.5f),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            GridElement(
+                "Row composable",
+                "A layout composable that places its children in a horizontal sequence.",
+                Color(0xFFB69DF8),
+                Modifier.weight(0.5f)
+            )
+            GridElement(
+                "Column composable",
+                "A layout composable that places its children in a vertical sequence.",
+                Color(0xFFF6EDFF),
+                Modifier.weight(0.5f)
+            )
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
