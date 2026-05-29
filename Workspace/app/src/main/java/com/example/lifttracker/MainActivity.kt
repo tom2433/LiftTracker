c7e0be6ba6e96ad7a7345a289c758849f081fd81
tom2433 <tom.2433.england@gmail.com>
2026-05-29 11:07:30 -0400

    TipTimeApp 2.2
    added total price after tip, and code cleanup

--- Workspace/app/src/main/java/com/example/lifttracker/MainActivity.kt
+++ Workspace/app/src/main/java/com/example/lifttracker/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -22,7 +21,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.*
+import androidx.compose.material.icons.rounded.AttachMoney
+import androidx.compose.material.icons.rounded.Percent
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -31,14 +31,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -103,6 +101,7 @@ fun TipTimeLayout() {
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
     val tip = calculateTip(amount, tipPercent, roundUp)
+    val total = NumberFormat.getCurrencyInstance().format(amount + (tip.toDoubleOrNull() ?: 0.0))
 
     Column(
         modifier = Modifier
@@ -164,8 +163,16 @@ fun TipTimeLayout() {
             modifier = Modifier.padding(bottom = 32.dp)
         )
 
+        // tip amount
         Text(
             text = stringResource(R.string.tip_amount, tip),
+            style = MaterialTheme.typography.displaySmall,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        // total + tip amount
+        Text(
+            text = stringResource(R.string.total_amount, total),
             style = MaterialTheme.typography.displaySmall
         )
 
@@ -224,123 +231,3 @@ fun TipTimePreview() {
         TipTimeLayout()
     }
 }
-
-//@Preview(showBackground = true)
-//@Composable
-//fun LemonadeApp() {
-//    LemonadeScreen()
-//}
-//
-//@Composable
-//fun LemonadeScreen(modifier: Modifier = Modifier) {
-//    Column(
-//        modifier = modifier.fillMaxSize(),
-//        verticalArrangement = Arrangement.Top,
-//        horizontalAlignment = Alignment.CenterHorizontally
-//    ) {
-//        // Lemonade header surface
-//        LemonadeHeader()
-//
-//        // Lemonade Activity Container (contains image and text)
-//        LemonadeActivityContainer(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .wrapContentSize(Alignment.Center)
-//        )
-//    }
-//}
-//
-//@Composable
-//fun LemonadeActivityContainer(modifier: Modifier = Modifier) {
-//    Column (
-//        modifier = modifier,
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//        verticalArrangement = Arrangement.Center
-//    ) {
-//        var step by remember { mutableIntStateOf(1) }
-//        var clickCount by remember { mutableIntStateOf((2..4).random()) }
-//        var currentClick by remember { mutableIntStateOf(0) }
-//
-//        // determine image and text from step
-//        var imageResource: Int
-//        var imageResourceDescription: String
-//        var instruction: String
-//        when (step) {
-//            1 -> {
-//                imageResource = R.drawable.lemon_tree
-//                instruction = stringResource(R.string.lemonade_instruction_1)
-//                imageResourceDescription =
-//                    stringResource(R.string.lemonade_image_content_description_1)
-//            }
-//            2 -> {
-//                imageResource = R.drawable.lemon_squeeze
-//                instruction = stringResource(R.string.lemonade_instruction_2)
-//                imageResourceDescription =
-//                    stringResource(R.string.lemonade_image_content_description_2)
-//            }
-//            3 -> {
-//                imageResource = R.drawable.lemon_drink
-//                instruction = stringResource(R.string.lemonade_instruction_3)
-//                imageResourceDescription =
-//                    stringResource(R.string.lemonade_image_content_description_3)
-//            }
-//            else -> {
-//                imageResource = R.drawable.lemon_restart
-//                instruction = stringResource(R.string.lemonade_instruction_4)
-//                imageResourceDescription =
-//                    stringResource(R.string.lemonade_image_content_description_4)
-//            }
-//        }
-//
-//        Button(
-//            onClick = {
-//                if (step == 4) {
-//                    step = 1
-//                } else if (step == 2) {
-//                    currentClick++
-//                    if (currentClick == clickCount) {
-//                        step++
-//                        currentClick = 0
-//                        clickCount = (2..4).random()
-//                    }
-//                } else {
-//                    step++
-//                }
-//            },
-//            shape = RoundedCornerShape(40.dp)
-//        ) {
-//            Image(
-//                painter = painterResource(imageResource),
-//                contentDescription = imageResourceDescription
-//            )
-//        }
-//        Spacer(modifier = Modifier.height(16.dp))
-//        Text(
-//            text = instruction,
-//            textAlign = TextAlign.Center
-//        )
-//    }
-//}
-//
-//@Composable
-//fun LemonadeHeader(modifier: Modifier = Modifier) {
-//    Surface(
-//        modifier = modifier
-//            .fillMaxWidth()
-//            .height(100.dp),
-//        color = Color(249, 228, 75)
-//    ) {
-//        // Box to align header text
-//        Box(
-//            modifier = Modifier.padding(12.dp),
-//            contentAlignment = BottomCenter
-//        ) {
-//            Text(
-//                text = stringResource(R.string.lemonade),
-//                fontWeight = FontWeight.SemiBold,
-//                fontSize = 20.sp,
-//                color = Color(0, 0, 0)
-//            )
-//        }
-//    }
-//}
