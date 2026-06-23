dd77154fcaab3b1c49d012740b84148aef98c5d2
tom2433 <tom.2433.england@gmail.com>
2026-06-23 08:56:33 -0400

    attempted bug fix
    Issue: The app crashes whenever deleting an item.
    
    cause: when the item is deleted, the getItem() for the itemdetails screen returns null, so the item details screen can no longer refresh, so the entire thing crashes.
    
    resolution: the ItemDao can now support nullable Items for when the getItem() for the ItemDetailsScreen is null due to a deletion.

--- Workspace/app/src/main/java/com/example/lifttracker/data/ItemDao.kt
+++ Workspace/app/src/main/java/com/example/lifttracker/data/ItemDao.kt
@@ -25,7 +25,7 @@ interface ItemDao {
     suspend fun delete(item: Item)
 
     @Query("SELECT * FROM items WHERE id = :id")
-    fun getItem(id: Int): Flow<Item>
+    fun getItem(id: Int): Flow<Item?>
 
     @Query("SELECT * FROM items ORDER BY name ASC")
     fun getAllItems(): Flow<List<Item>>
