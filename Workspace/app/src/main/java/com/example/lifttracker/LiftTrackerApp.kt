b354aece9c2b243131436bf9adf9ab291db0c876
tom2433 <tom.2433.england@gmail.com>
2026-06-24 11:14:56 -0400

    F2-1.0.1: Modified drawer sheet open/close animation
    Removed the ModalNavigationDrawer wrapper and replaced it with AnimatedVisibility which slides in and out using a tween animationSpec. Added pointerInput to the Modifier on the ModalDrawerSheet to allow user to drag it in and out. Scrim color on the background is achieved with another AnimatedVisibililty with a box using a lower z-index than the ModalDrawerSheet.

--- Workspace/app/src/main/java/com/example/lifttracker/LiftTrackerApp.kt
+++ Workspace/app/src/main/java/com/example/lifttracker/LiftTrackerApp.kt
@@ -1,42 +1,55 @@
 package com.example.lifttracker
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarMonth
-import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material.icons.filled.FitnessCenter
 import androidx.compose.material.icons.filled.History
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.PublishedWithChanges
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.DrawerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.lifttracker.ui.navigation.LiftTrackerNavHost
-import kotlinx.coroutines.launch
 
 /**
  * Top level composable that represents screens for the application
@@ -46,20 +59,100 @@ fun LiftTrackerApp(navController: NavHostController = rememberNavController()) {
     LiftTrackerNavHost(navController = navController)
 }
 
+
 /**
  * Modal Navigation drawer to be displayed unconditionally
+ *
+ * Will eventually need parameters for:
+ *  - sessionIsActive (Boolean)
+ *  - current active screen (enum?)
+ *  -
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LiftTrackerDrawer(
     content: @Composable (PaddingValues) -> Unit
 ) {
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
+    var isDrawerOpen by remember { mutableStateOf(false) }
+    var drawerDragAmount by remember { mutableFloatStateOf(0f) }
 
-    ModalNavigationDrawer(
-        drawerContent = {
-            ModalDrawerSheet {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(stringResource(R.string.app_name)) },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                isDrawerOpen = !isDrawerOpen
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = stringResource(R.string.menu)
+                            )
+                        }
+                    }
+                )
+            }
+        ) { innerPadding ->
+            content(innerPadding)
+        }
+
+        // scrim color animation
+        AnimatedVisibility(
+            visible = isDrawerOpen,
+            enter = fadeIn(animationSpec = tween(durationMillis = 700)),
+            exit = fadeOut(animationSpec = tween(durationMillis = 700)),
+            modifier = Modifier.zIndex(1f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(DrawerDefaults.scrimColor)
+                    .clickable {
+                        isDrawerOpen = false
+                    }
+            )
+        }
+
+        // modal drawer sheet animation
+        AnimatedVisibility(
+            visible = isDrawerOpen,
+            enter = slideInHorizontally(
+                initialOffsetX = { -it },
+                animationSpec = tween(durationMillis = 700)
+            ),
+            exit = slideOutHorizontally(
+                targetOffsetX = { -it },
+                animationSpec = tween(durationMillis = 700)
+            ),
+            modifier = Modifier.zIndex(2f)
+        ) {
+            ModalDrawerSheet(
+                // allow user to physically drag the drawer sheet in and out
+                modifier = Modifier.pointerInput(Unit) {
+                    detectHorizontalDragGestures(
+                        onDragStart = {
+                            drawerDragAmount = 0f
+                        },
+                        onHorizontalDrag = { _, dragAmount ->
+                            if (dragAmount < 0) {
+                                drawerDragAmount += dragAmount
+                            }
+                        },
+                        onDragEnd = {
+                            if (drawerDragAmount < -80.dp.toPx()) {
+                                isDrawerOpen = false
+                            }
+                            drawerDragAmount = 0f
+                        },
+                        onDragCancel = {
+                            drawerDragAmount = 0f
+                        }
+                    )
+                }
+            ) {
                 Column(
                     modifier = Modifier
                         .padding(
@@ -75,7 +168,7 @@ fun LiftTrackerDrawer(
                         style = MaterialTheme.typography.titleLarge
                     )
 
-                    // nav drawer element: Begin Session
+                    // nav drawer element: Begin Session (eventually dynamic for resume/quit session)
                     NavigationDrawerItem(
                         label = { Text(stringResource(R.string.begin_session_title)) },
                         selected = false,
@@ -93,6 +186,9 @@ fun LiftTrackerDrawer(
                     )
 
                     // nav drawer element: Muscle Groups
+                    // when clicked, expands to show dropdown of muscle group names/notes w/pencil
+                    // when a muscle group is clicked, expands to show dropdown of lift names/notes w/pencil
+                    // when a lift is clicked, expands to show dropdown of
                     NavigationDrawerItem(
                         label = { Text(stringResource(R.string.muscle_groups_title)) },
                         selected = false,
@@ -166,35 +262,6 @@ fun LiftTrackerDrawer(
                     )
                 }
             }
-        },
-        drawerState = drawerState
-    ) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text(stringResource(R.string.app_name)) },
-                    navigationIcon = {
-                        IconButton(
-                            onClick = {
-                                scope.launch {
-                                    if (drawerState.isClosed) {
-                                        drawerState.open()
-                                    } else {
-                                        drawerState.close()
-                                    }
-                                }
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.Menu,
-                                contentDescription = stringResource(R.string.menu)
-                            )
-                        }
-                    }
-                )
-            }
-        ) { innerPadding ->
-            content(innerPadding)
         }
     }
 }
