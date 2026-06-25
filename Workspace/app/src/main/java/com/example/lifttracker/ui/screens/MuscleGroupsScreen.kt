cc7c05876257dcac3479556c2b4cd5b84b0f969d
tom2433 <tom.2433.england@gmail.com>
2026-06-25 15:17:49 -0400

    F1_3.2: modified keyboard ImeAction
    made it so that the enter key is replaced with a checkmark which closes the keyboard

--- Workspace/app/src/main/java/com/example/lifttracker/ui/screens/MuscleGroupsScreen.kt
+++ Workspace/app/src/main/java/com/example/lifttracker/ui/screens/MuscleGroupsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Label
 import androidx.compose.material.icons.filled.Description
@@ -35,6 +36,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -185,6 +187,9 @@ fun WelcomeDialog(
                             contentDescription = stringResource(R.string.profile_name_input_label)
                         )
                     },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Done
+                    ),
                     modifier = Modifier
                         .padding(bottom = 16.dp)
                         .fillMaxWidth()
@@ -203,6 +208,9 @@ fun WelcomeDialog(
                             contentDescription = stringResource(R.string.profile_note_input_label)
                         )
                     },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Done
+                    ),
                     modifier = Modifier
                         .padding(bottom = 16.dp)
                         .fillMaxWidth()
